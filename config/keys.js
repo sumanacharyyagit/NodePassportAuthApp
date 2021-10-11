c071795cca6dfaf50c1000d535759bf08b7a9508
Suman Acharyya <42718046+sumanacharyyagit@users.noreply.github.com>
2021-10-12 02:39:29 +0530

    URI Fix

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://suman123:suman123@sacluster.fqsrk.mongodb.net/nodeAuthDB?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://suman123:<Give_Your_PASSWORD>@sacluster.fqsrk.mongodb.net/nodeAuthDB?retryWrites=true&w=majority'
 }
